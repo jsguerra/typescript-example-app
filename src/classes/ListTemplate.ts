0ca9bb8d289ab700749428ac942d55f3b38ca109
jsguerra <jose@gcostudios.com>
2021-09-10 18:58:30 +0100

    initial script added

--- src/classes/ListTemplate.ts
+++ src/classes/ListTemplate.ts
@@ -5,3 +5,29 @@
     -- create the html template (li, h4, p)
     -- add the 'li' template to the start/end of the list
 */
+
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+export class ListTemplate {
+  constructor(private container: HTMLUListElement) { }
+
+  render(item: HasFormatter, heading: string, pos: 'start' | 'end') {
+    const li = document.createElement('li')
+    const h4 = document.createElement('h4')
+
+    h4.innerText = heading
+    li.append(h4)
+
+    const p = document.createElement('p')
+
+    p.innerText = item.format()
+    li.append(p)
+
+    if (pos === 'start') {
+      this.container.prepend(li)
+    } else {
+      this.container.append(li)
+    }
+
+  }
+}
