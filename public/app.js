4b41f6a10edbb5efb4dfce34690525ea4632da18
jsguerra <jose@gcostudios.com>
2021-09-10 18:58:15 +0100

    file updated

--- public/app.js
+++ public/app.js
@@ -1,11 +1,39 @@
+import { Invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { Payment } from './classes/Payments.js';
+// let docOne: HasFormatter
+// let docTwo: HasFormatter
+// docOne = new Invoice('yoshi', 'web work', 250)
+// docTwo = new Payment('mario', 'plumbing work', 200)
+// let docs: HasFormatter[] = []
+// docs.push(docOne)
+// docs.push(docTwo)
+// console.log(docs)
+// const invOne = new Invoice('mario', 'work on the mario website', 250)
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 400)
+// let invoices: Invoice[] = []
+// invoices.push(invOne)
+// invoices.push(invTwo)
+// invoices.forEach(inv => {
+//   console.log(inv.client, inv.amount, inv.format())
+// })
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
-export {};
