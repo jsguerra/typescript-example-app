da2e97b8890e6a02078b56631e8fbfaf0cdcbc89
Jose Guerra <jose.guerra@dodsgroup.com>
2021-09-10 12:38:39 +0100

    file updated

--- public/app.js
+++ public/app.js
@@ -1,1 +1,23 @@
 "use strict";
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var form = document.querySelector('.new-item-form');
+// inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
